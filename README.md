93edbb8d19506535c74c3cffcb8bac83b4ae8bb4
zgpeace <zgpeace@gmail.com>
2015-08-25 22:38:10 +0800

    slot machine

---
+++ README.md
@@ -0,0 +1,12 @@
+# SlotMachine
+##旋轉吧！使用UIPickerView元件與Emoji繪文字打造拉霸機App
+###在iOS程式中，UIPickerView通過滾輪界面提供單項或多值選項。這篇Swift教學會為你介紹UIPickerView元件，透過打造一個Emoji拉霸機App，你將會透徹瞭解如何使用UIPickerView。
+
+## Where I learn? You can do it absolutely.
+learn from raywenderlish, add auto layout by myself.  
+step by step with the tutorial below.  
+* [旋轉吧！使用UIPickerView元件與Emoji繪文字打造拉霸機App](http://www.appcoda.com.tw/uipickerview-tutorial/?utm_source=sendy&utm_medium=email&utm_campaign=uipickerview-intro) [APPCODA]
+
+## The result pictures show.
+
+ <img src="./images/emoji-slotmachine.gif" width = "281" height = "500" alt="图片名称" align=center />  
--- SlotMachine/ViewController.swift
+++ SlotMachine/ViewController.swift
@@ -8,12 +8,87 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UINavigationControllerDelegate,UIPickerViewDelegate,UIPickerViewDataSource {
 
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var imageArray = [String]()
+    var dataArray1 = [Int]()
+    var dataArray2 = [Int]()
+    var dataArray3 = [Int]()
+    
+    @IBAction func buttonClicked(sender: AnyObject) {
+        pickerView.selectRow(Int(arc4random_uniform(1000))%94 + 3, inComponent: 0, animated: true)
+        pickerView.selectRow(Int(arc4random_uniform(1000))%94 + 3, inComponent: 1, animated: true)
+        pickerView.selectRow(Int(arc4random_uniform(1000))%94 + 3, inComponent: 2, animated: true)
+        
+        if(dataArray1[pickerView.selectedRowInComponent(0)] == dataArray2[pickerView.selectedRowInComponent(1)] && dataArray1[pickerView.selectedRowInComponent(1)] == dataArray2[pickerView.selectedRowInComponent(2)]) {
+            resultLabel.text = "Bingo!!"
+        } else {
+            resultLabel.text = ""
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        imageArray = ["🍎","😍","🐮","🐼","🐔","⛄️","🚘","❤️","🎂","👻"]
+        
+        for(var i = 0; i < 100; i++)
+        {
+            dataArray1.append((Int)(arc4random_uniform(100) % 10))
+            dataArray2.append((Int)(arc4random_uniform(100) % 10))
+            dataArray3.append((Int)(arc4random_uniform(100) % 10))
+        }
+        
+        resultLabel.text = ""
+//        pickerView.delegate = self
+//        pickerView.dataSource = self
+
+        
     }
+    
+    func pickerView(pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        return 100.0
+    }
+    
+    func pickerView(pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 100.0
+    }
+    
+    func pickerView(pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusingView view: UIView!) -> UIView {
+        let pickerLabel = UILabel()
+        
+        if component == 0{
+            pickerLabel.text = imageArray[(Int)(dataArray1[row])]
+        } else if component == 1 {
+            pickerLabel.text = imageArray[(Int)(dataArray2[row])]
+        } else {
+            pickerLabel.text = imageArray[(Int)(dataArray3[row])]
+        }
+        
+        pickerLabel.font = UIFont(name: "Arial-BoldMT", size: 80)
+        pickerLabel.textAlignment = NSTextAlignment.Center
+        
+        return pickerLabel
+    }
+    
+    
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 100
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -22,4 +97,33 @@ class ViewController: UIViewController {
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
